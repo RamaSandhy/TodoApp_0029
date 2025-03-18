47e0bd8dfdbbbef0bce105152bf714255dd7597b
RamaSandhy <rshandy07@gmail.com>
2025-03-18 13:52:03 +0700

    menambahkan fungsi format tanggal untuk tugas

--- lib/task_page.dart
+++ lib/task_page.dart
@@ -88,6 +88,10 @@ void toggleTaskStatus(int index) {
     });
   }
 
+String _formatDate(DateTime date) {
+    return '${date.day.toString().padLeft(2, '0')}-${date.month.toString().padLeft(2, '0')}-${date.year} ${date.hour.toString().padLeft(2, '0')}:${date.minute.toString().padLeft(2, '0')}';
+  }
+
 
 
 
